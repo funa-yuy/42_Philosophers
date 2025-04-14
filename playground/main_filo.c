5366792d6c142e49ee4287402941a335811652cf
funa <orz.home35@gmail.com>
2025-04-14 19:25:20 +0900

    🛠️ 引数からusleepして、msにタイムスタンプ表示するようにした

--- playground/main_filo.c
+++ playground/main_filo.c
@@ -1,4 +1,3 @@
-/* マルチスレッドによりデータ競合が起きる流れ */
 
 #include <pthread.h>
 #include <stdio.h>
@@ -7,83 +6,81 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-// int pthread_join(pthread_t thread, void **retval);
-// int pthread_create(pthread_t *thread, const pthread_attr_t *attr, \
-// 				void *(*start_routine) (void *), void *arg);
-// int gettimeofday(struct timeval *tv, struct rtimezone *tz);
+#define UNIT_CONV 1000
 
+typedef struct	s_univ_rules
+{
+	int	total_philo;
+	int	time_die;
+	int	time_eat;
+	int	time_sleep;
+	int	must_eat;
+}	t_univ_rules;
 
 typedef struct	s_thread_arg
 {
 	pthread_t		thread_id;
-	int				num;
+	int				num_philo;
 	pthread_mutex_t	*mutex;
+	t_univ_rules	u_rules;
 }	t_thread_arg;
-
-typedef struct	s_uni_rules
-{
-	int	num_philo;
-	int	time_die;
-	int	time_eat;
-	int	time_sleep;
-	int	num_must_eat;
-}	t_uni_rules;
 
 void	*action_philosophers(void *arg)
 {
 	t_thread_arg	*data;
-	struct timeval	tv1;
-	struct timeval	tv2;
+	t_univ_rules	rules;
+	struct timeval	tv;
 	long			tv1_ms;
 	long			tv2_ms;
 
 	data = arg;
+	rules = data->u_rules;
+	printf("num_philo = %d \n", data->num_philo);
 
-	printf("num = %d \n", data->num);
+	gettimeofday(&tv, NULL);
+	tv1_ms = tv.tv_sec * UNIT_CONV + tv.tv_usec / UNIT_CONV;
+	printf("time stamp1(ms): \x1b[32m%ld\x1b[39m\n", tv1_ms);
 
-	gettimeofday(&tv1, NULL);
-	tv1_ms = tv1.tv_sec * 1000L + tv1.tv_usec / 1000L;
-	printf("time stamp1: %ld\n", tv1_ms);
+	usleep(rules.time_eat * UNIT_CONV);
 
-	usleep(1000000);
-
-	gettimeofday(&tv2, NULL);
-	tv2_ms = tv2.tv_sec * 1000L + tv2.tv_usec / 1000L;
-	printf("time stamp2: %ld, diff: %ld\n", tv2_ms, tv2_ms - tv1_ms);
+	gettimeofday(&tv, NULL);
+	tv2_ms = tv.tv_sec * UNIT_CONV + tv.tv_usec / UNIT_CONV;
+	printf("time stamp2(ms): \x1b[32m%ld\x1b[39m, diff: \x1b[32m%ld\x1b[39m\n", tv2_ms, tv2_ms - tv1_ms);
 
 	return (NULL);
 }
 
-t_uni_rules	init_uni_rules(int argc, char *argv[])
+t_univ_rules	init_univ_rules(int argc, char *argv[])
 {
-	t_uni_rules	rules;
+	t_univ_rules	rules;
 
-	rules.num_philo = atoi(argv[1]);
+	rules.total_philo = atoi(argv[1]);
 	rules.time_die = atoi(argv[2]);
 	rules.time_eat = atoi(argv[3]);
 	rules.time_sleep = atoi(argv[4]);
 	if (argc == 6)
-		rules.num_must_eat = atoi(argv[5]);
+		rules.must_eat = atoi(argv[5]);
 	else
-		rules.num_must_eat = -1;
+		rules.must_eat = -1;
 	return (rules);
 }
 
-void	mulch_thread(t_uni_rules rules)
+void	mulch_thread(t_univ_rules rules)
 {
 	t_thread_arg	*arg;
 	void			*retval;
 	pthread_mutex_t	mutex;
-	int			i;
+	int				i;
 	int				s;
 
-	arg = calloc(rules.num_philo, sizeof(t_thread_arg));
+	arg = calloc(rules.total_philo, sizeof(t_thread_arg));
 	pthread_mutex_init(&mutex, NULL);
 	i = 0;
-	while (rules.num_philo > i)
+	while (rules.total_philo > i)
 	{
 		arg[i].mutex = &mutex;
-		arg[i].num = i;
+		arg[i].num_philo = i;
+		arg[i].u_rules = rules;
 		printf("thread%dを作成 \n", i);
 		s = pthread_create(&arg[i].thread_id, NULL, action_philosophers, &arg[i]);
 		if (s != 0)
@@ -93,9 +90,8 @@ void	mulch_thread(t_uni_rules rules)
 
 	pthread_mutex_destroy(&mutex);
 
-	/* thread_idのスレッドの返り値を取得する */
 	i = 0;
-	while (rules.num_philo > i)
+	while (rules.total_philo > i)
 	{
 		s = pthread_join(arg[i].thread_id, &retval);
 		if (s != 0)
@@ -108,17 +104,17 @@ void	mulch_thread(t_uni_rules rules)
 
 int	main(int argc, char *argv[])
 {
-	t_uni_rules	rules;
+	t_univ_rules	rules;
 
 	if (argc > 6 || argc < 5)
 		return (1);
-	rules = init_uni_rules(argc, argv);
+	rules = init_univ_rules(argc, argv);
 
-	// printf("Philosophers: %d\n", rules.num_philo);
-	// printf("Time to die: %d\n", rules.time_die);
-	// printf("Time to eat: %d\n", rules.time_eat);
-	// printf("Time to sleep: %d\n", rules.time_sleep);
-	// printf("Each must eat: %d\n", rules.num_must_eat);
+	printf("Philosophers: %d\n", rules.total_philo);
+	printf("Time to die: %d\n", rules.time_die);
+	printf("Time to eat: %d\n", rules.time_eat);
+	printf("Time to sleep: %d\n", rules.time_sleep);
+	printf("Each must eat: %d\n", rules.must_eat);
 
 	mulch_thread(rules);
 
