02466f442e58af763a596cec2bf5032bddbe852f
funa <orz.home35@gmail.com>
2025-04-19 23:03:52 +0900

    🚧 right_forkとleft_forkをそれぞれfirstとsecondにした。その他、リファクタリングした

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:53:30 by miyuu             #+#    #+#             */
-/*   Updated: 2025/04/19 22:04:11 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/04/19 22:54:48 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,11 @@ typedef struct s_thread_arg
 {
 	pthread_t		thread_id;
 	int				philo_id;
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	*first_fork;
+	pthread_mutex_t	*second_fork;
 	/* TODO: 以下2つ、最終的に消す */
-	int				left_fork_n;
-	int				right_fork_n;
+	int				first_fork_n;
+	int				second_fork_n;
 	long			start_tv_ms;
 	long			*last_eat_time;
 	bool			*is_philo_die;
@@ -64,8 +64,10 @@ typedef struct s_die_judge
 t_univ_rules	init_univ_rules(int argc, char *argv[]);
 long			printf_philo_status(char *status, long s_time, int n_philo, \
 									long last_time);
+// void			init_thread_arg(t_thread_arg *arg, int id, t_univ_rules rules, pthread_mutex_t *forks, \
+// 								long start_tv_ms, long *last_eat_time, bool *is_philo_die);
 void			init_thread_arg(t_thread_arg *arg, int id, t_univ_rules rules, pthread_mutex_t *forks, \
-								long start_tv_ms, long *last_eat_time, bool *is_philo_die);
+						long start_tv_ms, long *last_eat_time, bool *is_philo_die);
 void			init_die_judge(t_die_judge	*die_judge, t_univ_rules rules, \
 								long *last_eat_time, bool *is_philo_die);
 void			lets_go_mulch_thread(t_univ_rules rules);
