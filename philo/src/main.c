7f3a9b33d0ab4fa04d515dde8e7239f35b52f097
funa <orz.home35@gmail.com>
2025-04-21 22:18:30 +0900

    ♻️ sleepの処理を別関数に分けた

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:57:59 by miyuu             #+#    #+#             */
-/*   Updated: 2025/04/21 22:13:46 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/04/21 22:18:03 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	eat_action(t_thread_arg *data, t_univ_rules rules, int *eat_num)
 		return (-1);
 	}
 	printf_philo_status("is eating", *data->start_tv_ms, \
-								data->philo_id + 1, 0);
+							data->philo_id + 1, 0);
 	*data->last_eat_time = get_now_time_ms();
 	safe_usleep(rules.time_eat_ms);
 	put_forks(data);
@@ -43,6 +43,17 @@ int	eat_action(t_thread_arg *data, t_univ_rules rules, int *eat_num)
 		return (-1);
 	if (rules.must_eat != -1 && ++(*eat_num) >= rules.must_eat)
 		*data->is_eat_full = true;
+	return (0);
+}
+
+/* eatが終わり、sleepを開始 */
+int	sleep_action(t_thread_arg *data, t_univ_rules rules)
+{
+	if (*data->can_stop_thread)
+		return (-1);
+	printf_philo_status("is sleeping", *data->start_tv_ms, \
+							data->philo_id + 1, 0);
+	safe_usleep(rules.time_sleep_ms);
 	return (0);
 }
 
@@ -64,12 +75,8 @@ void	*action_philosophers(void *arg)
 	{
 		if (eat_action(data, rules, &eat_num) != 0)
 			break ;
-
-		if (*data->can_stop_thread)
+		if (sleep_action(data, rules) != 0)
 			break ;
-		/* eatが終わり、sleepを開始 */
-		now_ms = printf_philo_status("is sleeping", *data->start_tv_ms, data->philo_id + 1, now_ms);
-		safe_usleep(rules.time_sleep_ms);
 
 		if (*data->can_stop_thread)
 			break ;
