45496c4a38027b4fbec9c4e33e2f1c701ae30fce
funa <orz.home35@gmail.com>
2025-04-27 17:01:35 +0900

    🏥 タイムスタンプが順番通りに出力できるようにした

--- philo/src/printf_philo_status.c
+++ philo/src/printf_philo_status.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 19:28:46 by miyuu             #+#    #+#             */
-/*   Updated: 2025/04/27 14:49:12 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/04/27 17:00:15 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ void	printf_philo_status(char *status, t_thread_arg *data, int n_philo)
 	long	now_ms;
 	long	s_time;
 
+	pthread_mutex_lock(&data->mutex->write_mutex);
+
 	pthread_mutex_lock(&data->mutex->start_tv_mutex);
 	s_time = *data->start_tv_ms;
 	pthread_mutex_unlock(&data->mutex->start_tv_mutex);
 
 	now_ms = get_now_time_ms();
-
-	pthread_mutex_lock(&data->mutex->write_mutex);
 
 	pthread_mutex_lock(&data->mutex->thread_mutex);
 	if (!*data->can_stop_thread)
