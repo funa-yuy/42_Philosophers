14c65f6eacafd13e38db97ff40cdde8214f3fd09
funa <orz.home35@gmail.com>
2025-04-27 19:48:04 +0900

    🏥 哲学者の数が0の時と、must_eatが0の時の処理を追加

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:57:59 by miyuu             #+#    #+#             */
-/*   Updated: 2025/04/21 22:23:40 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/04/27 19:47:01 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int	main(int argc, char *argv[])
 	}
 	if (init_univ_rules(&rules, argc, argv) != 0)
 		return (1);
+	if (rules.must_eat == 0)
+		return (0);
 	if (mulch_thread(rules) != 0)
 		return (1);
 	return (0);
